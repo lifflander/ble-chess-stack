a0b2ecff3d8b91453e704dc42c71aa695833ab5f
Jonathan Lifflander <jliffla@sandia.gov>
2023-06-09 00:13:57 -0700

    reformat

--- chess-app/chess-ui/src/App.tsx
+++ chess-app/chess-ui/src/App.tsx
@@ -19,9 +19,9 @@ function App() {
     const [games, setGames] = useState<Game[]>([])
 
     useEffect(() => {
-	fetch("http://localhost:5000/test/")
-	    .then((res) => res.json())
-	    .then((data) => setMessage(data.message));
+        fetch("http://localhost:5000/test/")
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
     }, []);
 
     const [title, setTitle] = useState('');
@@ -29,57 +29,57 @@ function App() {
     // const [posts, setPosts] = useState([]);
 
     const handleSubmit = (e : React.FormEvent<HTMLFormElement> ) => {
-	console.log("called submit");
-	e.preventDefault();
-	addGame(title);
+        console.log("called submit");
+        e.preventDefault();
+        addGame(title);
     };
 
     const addGame = async (title : String) => {
-	try {
-	    console.log("adding game");
-	    let response = await client.post('', {
-		title: title
-	    });
-	    console.log(response)
-	    getGames()
-	} catch (error) {
-	    console.log(error);
-	}
+        try {
+            console.log("adding game");
+            let response = await client.post('', {
+                title: title
+            });
+            console.log(response)
+            getGames()
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     const getGames = () => {
-	client.get('').then(json => setGames(json.data))
+        client.get('').then(json => setGames(json.data))
     }
 
     useEffect(() => {
-	getGames()
+        getGames()
     }, [])
 
     const renderTable = () => {
-	return games.map(game => {
-	    return (
-		<tr>
-		    <td>{game.id}</td>
-		    <td>{game.title}</td>
-		</tr>
-	    )
-	})
+        return games.map(game => {
+            return (
+                <tr>
+                    <td>{game.id}</td>
+                    <td>{game.title}</td>
+                </tr>
+            )
+        })
     }
 
 
     return (
-	<div className="App">
-	    <header className="App-header">
+        <div className="App">
+            <header className="App-header">
               <p>
               OK. {message}
               </p>
-	      <div className="add-game-container">
-	        <form onSubmit={handleSubmit}>
-	           <input type="text" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} />
-	           <button type="submit">Add Game</button>
-	        </form>
-	      </div>
-	      <table id="games">
+              <div className="add-game-container">
+                <form onSubmit={handleSubmit}>
+                   <input type="text" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} />
+                   <button type="submit">Add Game</button>
+                </form>
+              </div>
+              <table id="games">
               <thead>
                 <tr>
                   <th>ID</th>
@@ -87,10 +87,11 @@ function App() {
                 </tr>
               </thead>
               <tbody>{renderTable()}</tbody>
-	      </table>
-	    </header>
-	</div>
+              </table>
+            </header>
+        </div>
     );
 }
 
 export default App;
+
