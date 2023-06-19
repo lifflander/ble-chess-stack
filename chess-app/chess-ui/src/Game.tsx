a92cb7d0fd4d84f3b18ef91ecec67c095c3a45a9
Jonathan Lifflander <jliffla@sandia.gov>
2023-06-18 23:34:55 -0700

    check for game over

--- chess-app/chess-ui/src/Game.tsx
+++ chess-app/chess-ui/src/Game.tsx
@@ -64,11 +64,13 @@ function Game() {
             const s : Chess = new Chess()
             game.moves.map((move) => {
                 if (move.moveIndex < moveNum) {
-                    s.move({
-                        from: move.pgn.substring(0, 2) as Square,
-                        to: move.pgn.substring(2, 4) as Square,
-                        promotion: "q"
-                    })
+                    if (!s.isGameOver) { 
+                        s.move({
+                            from: move.pgn.substring(0, 2) as Square,
+                            to: move.pgn.substring(2, 4) as Square,
+                            promotion: "q"
+                        })
+                    }
                 }
             })
             setChessState(s)
@@ -87,11 +89,13 @@ function Game() {
     const getPGNs = (moves : GameMove[]) : string[] => {
         const s : Chess = new Chess()
         moves.map((move) => {
-            s.move({
-                from: move.pgn.substring(0, 2) as Square,
-                to: move.pgn.substring(2, 4) as Square,
-                promotion: "q"
-            })
+            if (!s.isGameOver) { 
+                s.move({
+                    from: move.pgn.substring(0, 2) as Square,
+                    to: move.pgn.substring(2, 4) as Square,
+                    promotion: "q"
+                })
+            }
         })
         const history : Move[] = s.history({verbose: true})
         return history.map((h) => h.san);
