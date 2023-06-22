05cb860c3958ab1344576c1073265f7398f699e5
Jonathan Lifflander <jliffla@sandia.gov>
2023-06-22 14:50:39 -0700

    clean up entry page

--- chess-app/chess-ui/src/App.tsx
+++ chess-app/chess-ui/src/App.tsx
@@ -20,6 +20,9 @@ interface Game {
     id: number
     title: string
     moves: Move[]
+    createdAt: Date
+    whiteName: string
+    blackName: string
 };
 
 function App() {
@@ -55,22 +58,67 @@ function App() {
     };
 
     const getGames = async () => {
-        await client.get('games').then(json => setGames(json.data) )
+        await client.get('games').then((json) => {
+            var allgames = json.data as Game[]
+            allgames.sort((a : Game, b : Game) => { return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime() })
+
+            let nonzero = allgames.filter(g => g.moves.length != 0)
+            let zero = allgames.filter(g => g.moves.length == 0)
+            let nonzeroFilled = nonzero.filter(g => g.whiteName && g.whiteName !== "" && g.blackName && g.blackName !== "")
+            setGames(allgames)
+        })
     }
 
     useEffect(() => {
         getGames()
     }, [])
 
-    const renderTable = () => {
-        return games.map((game, index : number) => {
-            //let move_str : string = game.moves?.map((move : Move) : string => move.pgn).reduce((acc  : string, cur : string) : string => acc + cur, "" as string)
-            let move_len_str : string = "Moves: " + game.moves?.length
+    const renderTableDone = () => {
+        let nonzero = games.filter(g => g.moves.length != 0)
+        let nonzeroFilled = nonzero.filter(g => g.whiteName && g.whiteName !== "" && g.blackName && g.blackName !== "")
+        return nonzeroFilled.map((game, index : number) => {
             return (
                 <tr>
                     <td>{game.id}</td>
                     <td>{game.title}</td>
-                    <td>{move_len_str}</td>
+                    <td>{game.moves?.length}</td>
+                    <td>{game.whiteName + " vs. " + game.blackName}</td>
+                    <td>{new Date(game.createdAt).toLocaleString()}</td>
+                    <td>
+                      <a href={"/games/" + game.id}>View</a>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    const renderTableZero = () => {
+        let zero = games.filter(g => g.moves.length == 0)
+        return zero.map((game, index : number) => {
+            return (
+                <tr>
+                    <td>{game.id}</td>
+                    <td>{game.title}</td>
+                    <td>{game.moves?.length}</td>
+                    <td>{new Date(game.createdAt).toLocaleString()}</td>
+                    <td>
+                      <a href={"/games/" + game.id}>View</a>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    const renderTableOther = () => {
+        let nonzero = games.filter(g => g.moves.length != 0)
+        let nonzeroUnfilled = nonzero.filter(g => !(g.whiteName && g.whiteName !== "" && g.blackName && g.blackName !== ""))
+        return nonzeroUnfilled.map((game, index : number) => {
+            return (
+                <tr>
+                    <td>{game.id}</td>
+                    <td>{game.title}</td>
+                    <td>{game.moves?.length}</td>
+                    <td>{new Date(game.createdAt).toLocaleString()}</td>
                     <td>
                       <a href={"/games/" + game.id}>View</a>
                     </td>
@@ -82,12 +130,12 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-              <div className="add-game-container">
+              {/* <div className="add-game-container">
                 <form onSubmit={handleSubmit}>
                    <input type="text" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} />
                    <button type="submit">Add Game</button>
                    </form>
-              </div>
+              </div> */}
 	      <div className="table-holder">
               <table id="games" className="table table-sm Table-color">
               <thead className="thead-dark">
@@ -95,10 +143,38 @@ function App() {
                   <th>ID</th>
                   <th>Title</th>
                   <th>Moves</th>
+                  <th>Name</th>
+                  <th>Created</th>
                   <th></th>
                 </tr>
               </thead>
-              <tbody>{renderTable()}</tbody>
+              <tbody>{renderTableDone()}</tbody>
+              </table>
+
+              <table id="games" className="table table-sm Table-color">
+              <thead className="thead-dark">
+                <tr>
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th>Moves</th>
+                  <th>Created</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>{renderTableOther()}</tbody>
+              </table>
+
+              <table id="games" className="table table-sm Table-color">
+              <thead className="thead-dark">
+                <tr>
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th>Moves</th>
+                  <th>Created</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>{renderTableZero()}</tbody>
               </table>
 	    </div>
             </header>
